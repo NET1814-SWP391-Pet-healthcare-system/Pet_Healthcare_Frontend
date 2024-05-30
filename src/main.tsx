5ae721da58944f10f1c90082d3734a7fe3f40674
Nguyen Dinh Duong <DuongNDSE183456@fpt.edu.vn>
2024-05-30 07:50:15 +0700

    update nextui

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.ts'
+
+// 1. import `NextUIProvider` component
+import {NextUIProvider} from "@nextui-org/react";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  </Provider>
+  <NextUIProvider>
+    <Provider store={store}>
+      <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    </Provider>
+  </NextUIProvider>
 )
